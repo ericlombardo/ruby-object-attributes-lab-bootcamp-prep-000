35efaeb36efed54966d9d1ac54d67f361105547d
Eric Lombardo <eric.m.lombardo@gmail.com>
2020-10-21 18:20:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,18 @@
+# creates class for Dog
 class Dog
+  # creates writer for name attribute
   def name=(name)
     @name = name
   end
+  # creates reader for name attribute
   def name
     name = @name
   end
+  # creates writer for breed attribute
   def breed=(breed)
     @breed = breed
   end
+  # creates reader for breed attribute
   def breed
     breed = @breed
   end
